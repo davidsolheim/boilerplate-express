5ec9b6e95626f93826cfb4b428e19da5331a6085
davidsolheim <david@solheimtech.com>
2024-01-02 13:39:50 +0000

    Initial Commit

--- myApp.js
+++ myApp.js
@@ -1,10 +1,19 @@
 var express = require('express');
 var app = express();
+console.log("Hello World");
+/*app.get("/", (req, res) => {
+  res.send("Hello Express");
+});*/
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/public/index.html");
+});
+app.use("/public", express.static(__dirname + "/public"));
 
-
-
-
-
+app.get("/json", (req, res) => {
+  res.json({
+    message: "Hello json"
+  });
+});
 
 
 
